8444da984cc370ad1230e9020a5c6774988df50a
Iago Teles <iagooteles@gmail.com>
2024-07-30 13:37:02 -0300

    fixed svg icons size

--- app/components/cms-icon/index.tsx
+++ app/components/cms-icon/index.tsx
@@ -1,12 +1,47 @@
-type CMSIconProps = {
-    icon: string;
-}
+const addStyleToSvg = (
+  svgString: string,
+  styles: React.CSSProperties
+): string => {
+  // Verificação do Ambiente de Execução:
+  if (typeof document === "undefined") {
+    return svgString;
+  }
 
-export const CMSIcon = ({ icon }: CMSIconProps) => {
-    return (
-        <div 
-        dangerouslySetInnerHTML={{
-            __html: icon
-        }}/>
-    )
-}
+  const div = document.createElement("div");
+  div.innerHTML = svgString.trim();
+
+  const svg = div.querySelector("svg");
+
+  if (!svg) {
+    throw new Error("Invalid SVG string");
+  }
+
+  // Adiciona os estilos ao elemento SVG
+  Object.keys(styles).forEach((key) => {
+    svg.style[key as any] = styles[key as keyof React.CSSProperties] as string;
+  });
+
+  return div.innerHTML;
+};
+
+type CMSIconProps = {
+  icon: string;
+  style?: React.CSSProperties;
+};
+
+export const CMSIcon = ({ icon, style }: CMSIconProps) => {
+  // Verifica se estamos no navegador
+  if (typeof document === "undefined") {
+    return <div dangerouslySetInnerHTML={{ __html: icon }} />;
+  }
+
+  const styledIcon = addStyleToSvg(icon, style || {});
+
+  return (
+    <div
+      dangerouslySetInnerHTML={{
+        __html: styledIcon,
+      }}
+    />
+  );
+};
--- app/components/pages/home/known-techs/known-tech.tsx
+++ app/components/pages/home/known-techs/known-tech.tsx
@@ -1,25 +1,23 @@
 import { CMSIcon } from "@/app/components/cms-icon";
-import { KnownTech as IKnownTech} from "@/app/types/projects";
+import { KnownTech as IKnownTech } from "@/app/types/projects";
 import { getRelativeTimeString } from "@/app/utils/get-relative-time";
 
 type KnownTechProps = {
-    tech: IKnownTech
-}
+  tech: IKnownTech;
+};
 
 export const KnownTech = ({ tech }: KnownTechProps) => {
-    const relativeTime = getRelativeTimeString(
-        new Date(tech.startDate), "pt-BR"
-    ).replace("há", "")
-    return ( 
-        <div className="p-6 rounded-lg bg-gray-600/20 flex flex-col gap-2 hover:bg-violet-800 transition-all">
-            <div className="flex items-center justify-between">
-                <p className="font-medium">{tech.name}</p>
-                <CMSIcon 
-                    icon={tech.iconSvg} 
-                />
-
-            </div>
-            <span>{relativeTime} de experiência</span>
-        </div>
-    )
-}
+  const relativeTime = getRelativeTimeString(
+    new Date(tech.startDate),
+    "pt-BR"
+  ).replace("há", "");
+  return (
+    <div className="p-6 rounded-lg bg-gray-600/20 flex gap-2 hover:bg-violet-800 transition-all justify-between">
+      <div className="flex flex-col gap-2 justify-between">
+        <p className="font-medium">{tech.name}</p>
+        <span>{relativeTime} de experiência</span>
+      </div>
+      <CMSIcon icon={tech.iconSvg} style={{ width: "50px", height: "50px" }} />
+    </div>
+  );
+};
